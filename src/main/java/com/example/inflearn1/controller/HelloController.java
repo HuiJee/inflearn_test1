3c81565fb327aa80cd97b2332a84856927bb16ff
huijee <hpwhj0815@naver.com>
2024-08-09 14:02:43 +0900

    update) MVC와 템플릿엔진

--- src/main/java/com/example/inflearn1/controller/HelloController.java
+++ src/main/java/com/example/inflearn1/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.example.inflearn1.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -17,4 +18,10 @@ public class HelloController {
         컨트롤러에서 리턴 값으로 문자를 반환하면 뷰 리졸버(viewResolver)가 화면을 찾아 처리함
         resources:templates/{ViewName}.html
     * */
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
 }
