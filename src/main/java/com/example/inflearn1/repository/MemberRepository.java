51e2616cf40195644fbb361076e928982298ffb3
huijee <hpwhj0815@naver.com>
2024-08-09 14:51:56 +0900

    create) 회원 도메인과 repository 만들기(DB 미정을 가정하고 interface 사용)

---
+++ src/main/java/com/example/inflearn1/repository/MemberRepository.java
@@ -0,0 +1,20 @@
+package com.example.inflearn1.repository;
+
+import com.example.inflearn1.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+
+    /*
+        findById나 findByName의 경우 null을 반환할 수 있음
+
+        이를 처리하는 방법 중 하나가, 그대로 반환하는 것보다 Optional로 감싸서 반환하는 것!
+    * */
+}
---
+++ src/main/java/com/example/inflearn1/repository/MemoryMemberRepository.java
@@ -0,0 +1,39 @@
+package com.example.inflearn1.repository;
+
+import com.example.inflearn1.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    // 동시성 문제가 있을 수 있으므로 공유되는 변수일 경우 ConcurrentHashMap을 써야 함
+    private static Map<Long, Member> store = new HashMap<>();
+    // 이 또한 동시성 문제를 위해 Atomic Long을 사용해야 함
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+        // member 객체를 돌면서 해당 name과 동일한 걸 하나 찾으면 바로 반환
+        // 끝까지 돌렸을 때 존재하지 않으면 Optional에 null이 담겨 반환
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
